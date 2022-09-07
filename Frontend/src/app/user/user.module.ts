9f5c868c6d72b905c66be19d392b8d517b0bb908
4ouR04 <amosmwongelah@gmail.com>
2022-09-07 14:21:55 +0300

    Generate and update user related components

--- Frontend/src/app/user/user.module.ts
+++ Frontend/src/app/user/user.module.ts
@@ -2,14 +2,32 @@ import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
 import {RouterModule, Routes } from '@angular/router';
+import { ParcelsComponent } from './parcels/parcels.component';
+import { ParcelDetailsComponent } from './parcel-details/parcel-details.component';
 
 const userRoutes: Routes = [
-  { path: '', component: UserComponent },
+  {
+    path: '', component: UserComponent,
+    children: [
+      {
+      path: 'parcels', component: ParcelsComponent
+    },
+      
+      {
+        path: 'parcel/details', component: ParcelDetailsComponent
+      }
+    ]
+  },
+  {
+    path:'parcels/parcel/details', redirectTo: 'parcel/details'
+  }
 ]
 
 @NgModule({
   declarations: [
-    UserComponent
+    UserComponent,
+    ParcelsComponent,
+    ParcelDetailsComponent
   ],
   imports: [
     CommonModule,RouterModule.forChild(userRoutes)
