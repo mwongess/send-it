6573c9979671cb4080582e7a49d176766c24e534
mwongess <amosmwongelah@gmail.com>
2023-05-20 17:10:12 +0300

    Update auth route

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -9,6 +9,11 @@ const routes: Routes = [
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
   {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full'
+  },
+  {
     path: 'admin/dashboard',
     canActivate: [AuthGuard],
     loadChildren: () =>
